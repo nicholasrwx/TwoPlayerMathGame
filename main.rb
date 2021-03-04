24090e0c2287fe90a222900b3ef5d3cfe28ee5c9
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-03-03 20:11:42 -0700

    refactor classes

--- main.rb
+++ main.rb
@@ -1,73 +1,50 @@
 require "./question"
 require "./player"
 
-
 class Game
 
-#start = initialize
-
   def initialize
-    #display
-    puts "initialize"
-
-
-    #  -creates 2 Players
-    @player1 =  Player.new
-    @player2 =  Player.new
-    @question =  Question.new
-
+      @player1 =  Player.new
+      @player2 =  Player.new
+      @question =  Question.new
   end
 
-  def start 
-  
-  
+  def start     
     while @player1.getter > 0 || @player2.getter > 0 do
-
-
-
       @result1 = @question.random_Question("Player 1:")
       @response1 = @player1.player_Answer("P1")
-    
       if @result1 == @response1  
-        puts "Player 1: YES! You are Correct!"
+         puts "Player 1: YES! You are Correct!"
       else
-        @player1.setter=(-1)
-        puts "Player 1: Seriously? No!"
+         @player1.setter=(-1)
+         puts "Player 1: Seriously? No!"
       end
-
           if @player1.getter == 0
-            puts "Player 2 wins with a score of #{@player2.getter}/3"
-            break
-          else  
-          puts "P1: #{@player1.getter}/3 vs P2: #{@player2.getter}/3"  
-          puts "----- NEW TURN -----"  
-
-          @result2 = @question.random_Question("Player 2:")
-          @response2 = @player2.player_Answer("P2")
-
-          if @result2 == @response2  
-          puts "Player 2: YES! You are Correct!"
-          else
-          @player2.setter=(-1)
-          puts "Player 2: Seriously? No!"
-          end
-
-          if @player2.getter == 0
-            puts "Player 1 wins with a score of #{@player1.getter}/3"
-            break
+             puts "Player 2 wins with a score of #{@player2.getter}/3"
+             break
           else  
             puts "P1: #{@player1.getter}/3 vs P2: #{@player2.getter}/3"  
             puts "----- NEW TURN -----"  
+            @result2 = @question.random_Question("Player 2:")
+            @response2 = @player2.player_Answer("P2")
+              if @result2 == @response2  
+                 puts "Player 2: YES! You are Correct!"
+              else
+                 @player2.setter=(-1)
+                 puts "Player 2: Seriously? No!"
+              end
+              if @player2.getter == 0
+                 puts "Player 1 wins with a score of #{@player1.getter}/3"
+                 break
+              else  
+                 puts "P1: #{@player1.getter}/3 vs P2: #{@player2.getter}/3"  
+                 puts "----- NEW TURN -----"  
+              end
           end
-        end
-      end
-
-      puts "----- GAMEOVER -----"
-      puts "GoodBye!"
-  
+    end
+    puts "----- GAMEOVER -----"
+    puts "GoodBye!"
   end
-
-
 end
 
 game = Game.new
--- player.rb
+++ player.rb
@@ -1,26 +1,23 @@
   
 class Player
-  #get a response from a player
-    def initialize
     
+  def initialize
       @lives = 3
+  end
   
-    end
-  
-  
-    def player_Answer(p)
+  def player_Answer(p)
       print "#{p}>"
       response = $stdin.gets.chomp.to_i     
-        return response
-    end
-  
-    def getter
-      @lives = @lives
-    end
-  
-    def setter=(x)
-      @lives = @lives + x 
-    end
-  
+      return response
   end
+  
+  def getter
+      @lives = @lives
+  end
+  
+  def setter=(x)
+      @lives = @lives + x 
+  end
+  
+end
   
